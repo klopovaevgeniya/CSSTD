1634374344f06a3ce33e77cc8ddfb5f62e49f4c8
klopovaevgeniya <isip_e.d.klopova@mpt.ru>
2026-04-28 18:34:47 +0300

    График доработан

--- core/views/manager/calendar.py
+++ core/views/manager/calendar.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from core.decorators import role_required
-from core.models import Employee, Project
+from core.models import Employee, Project, ProjectTask
 from core.utils.project_archive import archived_project_q
 
 
@@ -20,40 +20,47 @@ def manager_calendar(request):
     else:
         projects = Project.objects.filter(manager__employee_user=request.user).exclude(archived_project_q())
 
-    # Только проекты с датой окончания
-    projects_with_deadline = projects.exclude(end_date__isnull=True)
+    project_ids = list(projects.values_list('id', flat=True))
 
-    # Подготовка данных для календаря
-    project_events = []
-    statuses = set()
-    types = set()
+    tasks = ProjectTask.objects.filter(project_id__in=project_ids).exclude(
+        due_date__isnull=True
+    ).select_related('project')
 
-    for p in projects_with_deadline:
+    calendar_items = []
+
+    for p in projects.exclude(end_date__isnull=True):
         status_name = p.status.name if p.status else ''
         type_name = p.type.name if p.type else ''
-        statuses.add(status_name)
-        types.add(type_name)
 
-        project_events.append({
-            'id': p.id,
+        calendar_items.append({
+            'id': f'project-{p.id}',
             'title': p.name,
-            'start': p.end_date.isoformat() if p.end_date else None,
-            'end': p.end_date.isoformat() if p.end_date else None,
+            'start': p.end_date.isoformat(),
+            'end': p.end_date.isoformat(),
             'status': status_name,
             'type': type_name,
             'description': p.description or '',
             'code': p.code or '',
             'start_date': p.start_date.isoformat() if p.start_date else '',
+            'item_type': 'project',
+            'link': f'/manager/projects/{p.id}/detail/',
         })
 
-    # Сортируем для удобства отображения
-    sorted_statuses = sorted([s for s in statuses if s])
-    sorted_types = sorted([t for t in types if t])
+    for t in tasks:
+        project_name = t.project.name if t.project else ''
 
-    context = {
-        'projects_for_calendar': project_events,
-        'calendar_statuses': sorted_statuses,
-        'calendar_types': sorted_types,
-    }
+        calendar_items.append({
+            'id': f'task-{t.id}',
+            'title': t.name,
+            'start': t.due_date.isoformat(),
+            'end': t.due_date.isoformat(),
+            'status': t.status or '',
+            'type': 'Задача',
+            'description': t.description or '',
+            'code': project_name,
+            'start_date': '',
+            'item_type': 'task',
+            'link': f'/manager/tasks/{t.id}/',
+        })
 
-    return render(request, 'manager/calendar.html', context)
+    return render(request, 'manager/calendar.html', {'calendar_items': calendar_items})
